0d4deed3dae36322e4a5d4ccceb96dbda13bb81a
tylerhoecker <hoecker.tyler@gmail.com>
2023-12-18 16:30:39 -0700

    re-writing 01_ with coordinates; styling changes with vscode notes

--- code/01_calc_sigma.R
+++ code/01_calc_sigma.R
@@ -1,7 +1,7 @@
 #-------------------------------------------------------------------------------
 # Testing
-#historical <- rast('../data/climate/topoterra_hist_1961-1990.tif')
-#future <- rast('../data/climate/topoterra_2C_1985-2015.tif')
+# historical <- rast('../data/climate/topoterra_hist_1961-1990.tif')
+# future <- rast('../data/climate/topoterra_2C_1985-2015.tif')
 #-------------------------------------------------------------------------------
 
 # Packages and functions -------------------------------------------------------
@@ -11,140 +11,128 @@ library(tidyr)
 library(dplyr)
 library(furrr)
 library(purrr)
-source('as.data.table.r') # https://gist.github.com/etiennebr/9515738#file-as-data-table-r
+library(data.table)
+# https://gist.github.com/etiennebr/9515738#file-as-data-table-r
+source("code/as.data.table.r")
 
 # OR-WA boundary for crop ------------------------------------------------------
-climate_dir <- file.path('..','data','climate')
+climate_dir <- file.path("data", "climate")
 
-new_crs <- crs(rast(paste0(climate_dir,'/topoterra_2C_2000.tif')))
+new_crs <- crs(rast(paste0(climate_dir, "/topoterra_2C_2000.tif")))
 
-w_states <- read_sf('../data/western_states/western_states.shp') %>% 
-  st_transform(., new_crs)
+w_states <- read_sf("data/western_states/western_states.shp") |>
+  st_transform(new_crs)
 
-r6 <- w_states %>% 
-  filter(NAME %in% c('Washington'))
+r6 <- w_states  |>
+  filter(NAME %in% c("Colorado"))
 
 # Read in climate data, crop, convert to data.table ----------------------------
 # 2C annuals
-ann_fut <- grep('topoterra_2C_[0-9]{4}.tif', 
-                list.files(climate_dir), 
-                value = T) %>%
-  paste0(climate_dir,'/',.) %>% 
-  map(\(x) crop(rast(x),r6)) 
+ann_fut <- grep("topoterra_2C_[0-9]{4}.tif",
+                list.files(climate_dir),
+                value = TRUE) |>
+  paste0(climate_dir, "/", ... = _) |>
+  map(\(x) crop(rast(x), r6))
 
-ann_fut <- lapply(ann_fut, FUN = as.data.table)
+ann_fut <- lapply(ann_fut, FUN = as.data.table, xy = TRUE)
 
-# Record the coordinates of the cells, not preserved by datatable
-fut_coords <- paste0(climate_dir,'/','topoterra_2C_1985.tif') %>% 
-  rast() %>% 
-  crop(., r6) %>% 
-  crds()
 
 # Historical normals (downscaled from TerraClimate normals)
-norm_hist <- grep('topoterra_hist_[0-9]{4}-[0-9]{4}.tif', 
-                  list.files(climate_dir), 
-                  value = T) %>%
-  paste0(climate_dir,'/',.) %>% 
-  rast(.) %>% 
-  as.data.table(.) 
- 
-# Save out the centroid coordinates separately
-ref_coords <- grep('topoterra_hist_[0-9]{4}-[0-9]{4}.tif', 
-                   list.files(climate_dir), 
-                   value = T) %>%
-  paste0(climate_dir,'/',.) %>% 
-  rast(.) %>% 
-  crds()
+norm_hist <- grep("topoterra_hist_[0-9]{4}-[0-9]{4}.tif",
+                  list.files(climate_dir),
+                  value = TRUE) |>
+  paste0(climate_dir, "/", ... = _) |>
+  rast() |>
+  as.data.table(xy = TRUE)
 
 rm(new_crs, w_states, r6)
+
 gc()
 #-------------------------------------------------------------------------------
 
 # Calculate sigma dissimilarity between all focal locations and n best analogs
 #-------------------------------------------------------------------------------
 # How many analogs randomly sampled from global pool?
-n_analogs <- 1000000
-#pt_i = 100000
+n_analogs <- 10000000
+#pt_i  <- 1000
+#chunk  <- chunks[[100]]
+
 # The MD/sigma dissimilarity function
 md_fun <- function(pt_i){
   
-  print(paste0('Running point ', pt_i, ' of ', nrow(chunk[[1]])))
-  
-  # Build covariance matrix for pt_i from 30 years of annual projected future data
-  cov_i <- map(chunk, ~ .x[pt_i]) %>%
-    rbindlist() %>%
+  # Build cov matrix for pt_i from 30 years of annual projected future data
+  cov_i <- map(chunk, ~ .x[pt_i]) |>
+    rbindlist() |>
+    dplyr::select(-x, -y) |>
     cov()
- 
+
   # Calculate the mean of the future annuals -----------------------------------
-  # This provides different result than calculating the mean here... 
+  # This provides different result than calculating the mean here...
   # probably a result of the downscaling process
-  # fut_mu_vec <- norm_fut[pt_i] %>%
-  #   unlist()
-  fut_mean <- map(chunk, ~ .x[pt_i]) %>%
-    rbindlist() %>% 
-    summarise(across(c(aet, def, tmax, tmin), mean)) %>% 
+  fut_mean <- map(chunk, ~ .x[pt_i]) |>
+    rbindlist() |>
+    summarise(across(c(aet, def, tmax, tmin), mean)) |>
     unlist()
-  
+ 
   # Analog pool - from historical normals --------------------------------------
   # This *could* be done outside the function, but this is less biased, because
-  # random samples are drawn with each iteration, ultimately sampling from (probably)
+  # random samples are drawn with each iteration,
+  # ultimately sampling from (probably)
   # nearly the entire landscape
   
-  # This could be integrated with the chunk below, using slice_sample, 
+  # This could be integrated with the chunk below, using slice_sample,
   # but saving this index will be useful for extracting coordinates later
-  random_pts <- seq(nrow(norm_hist)) %>% 
+  random_pts <- seq_len(nrow(norm_hist)) |>
     sample(size = n_analogs)
   
-  ref_mat <- norm_hist[random_pts] %>% 
-    as.matrix() 
+  # Build reference matrix from random sample of historical normals
+  ref_mat <- norm_hist[random_pts, ] |>
+    dplyr::select(-x, -y) |>
+    as.matrix()
   
   # Sigma dissimilarity between pt_i and analog pool ---------------------------
   # Calculate 'raw' unsquared Mahalanobis distances
-  D <- mahalanobis(ref_mat, fut_mean, cov_i) 
+  d <- mahalanobis(ref_mat, fut_mean, cov_i)
   
-  # Mahoney follows a procedure where D is immediately unsquared, then is fed into 
-  # a chi distribution function. Mathematically, it is equivalent to feed the squared-D into 
-  # a chi-squared distribution function, and then unsquare the result. This is an order of 
-  # magnitude faster, because the chi::qchi function is very slow.
-  # %>% sqrt()
+  # Mahoney follows a procedure where D is immediately unsquared,
+  # then is fed into a chi distribution function.
+  # Mathematically, it is equivalent to feed the squared-D into
+  # a chi-squared distribution function, and then unsquare the result. 
+  # stats::chisq is an order of magnitude faster than chi::qchi.
   
-  # Convert distances (D) to percentiles of chi distribution (mulit-dimensional normal)
+  # Convert distances to percentiles of chi distribution (mulit-dimensional normal)
   # df = number of dimensions / climate variables
-  P <- pchisq(D, df = length(fut_mean)) 
+  p <- pchisq(d, df = length(fut_mean))
   # Convert percentiles into quantiles (standard deviations from mean)
-  sigma <- qchisq(P, df = 1) # df is now 1... 
-  sigma <- sqrt(sigma) %>% 
-    round(., 3)
+  sigma <- qchisq(p, df = 1) # df is now 1...
+  sigma <- sqrt(sigma) |>
+    round(3)
 
-  # Bind values to coordinates of best analogs
-  ref_coords_sample <- ref_coords[random_pts,]
+  # Save output
+  out_dt <- data.table("f_x" = chunk[[1]][pt_i, "x"],
+                       "f_y" = chunk[[1]][pt_i, "y"],
+                       "a_x" = norm_hist[random_pts, "x"],
+                       "a_y" = norm_hist[random_pts, "y"],
+                       "sigma" = sigma)
+  # Not sure why .x/.y get's appended above... data.table thing
+  setnames(out_dt,
+           old = c("f_x.x", "f_y.y", "a_x.x", "a_y.y"),
+           new = c("f_x", "f_y", "a_x", "a_y"))
 
-  # COORDS NOT EXPECTED RESULT - NEED TO FIX! - taking the same first rows rather than from chunk
-  out_dt <- data.table('fc_x' = fut_coords[pt_i,'x'],
-                       'fc_y' = fut_coords[pt_i,'y'],
-                       'an_x' = ref_coords_sample[,'x'],
-                       'an_y' = ref_coords_sample[,'y'],
-                       'md' = D,
-                       'sigma' = sigma) %>% 
-    filter(is.finite(sigma), sigma < 2) %>% 
-    mutate('dist_m' = sqrt((fc_x-an_x)^2 + (fc_y-an_y)^2)) 
-  
-  rm(cov_i, fut_mean, random_pts, ref_mat, D, P, sigma, ref_coords_sample)
+  out_dt <- out_dt |>
+    dplyr::filter(is.finite(sigma), sigma < 2) |>
+    mutate("dist_m" = round(sqrt((f_x - a_x)^2 + (f_y - a_y)^2))) |>
+    arrange(sigma) |>
+    slice_head(n = 1000)
+
+  rm(cov_i, fut_mean, random_pts, ref_mat, d, p, sigma)
   gc()
   return(out_dt)
 }
 
-# Temp plotting during development
-# some_pts <- out_dt %>%
-#   slice_sample(n = 10000)
-# 
-# ggplot(out_dt, aes(x = dist_m*0.001, y = sigma)) +
-#   geom_point(data = some_pts) +
-#   geom_smooth()
-
-# Break the focal dataset into chunks 
+# Break the focal dataset into chunks
 # ------------------------------------------------------------------------------
-# Set the number of chunks 
+# Set the number of chunks
 num_chunks <- 300
 # Get the total number of rows in each dataframe
 total_rows <- nrow(ann_fut[[1]])
@@ -158,43 +146,31 @@ for (i in 1:num_chunks) {
   # Calculate the start and end indices for each chunk
   start_index <- (i - 1) * rows_per_chunk + 1
   end_index <- min(i * rows_per_chunk, total_rows)
-  # Slice each dataframe in the list to get the subset of rows for the current chunk
-  chunk <- lapply(ann_fut, function(df) df[start_index:end_index, , drop = FALSE])
+  # Get the subset of rows for the current chunk
+  chunk <- lapply(ann_fut,
+                  function(df) df[start_index:end_index, , drop = FALSE])
   # Append the chunk to the list of chunks
   chunks[[i]] <- chunk
 }
-
-coord_chunks <- list()
-# Iterate over the range of chunks
-for (i in 1:num_chunks) {
-  # Calculate the start and end indices for each chunk
-  start_index <- (i - 1) * rows_per_chunk + 1
-  end_index <- min(i * rows_per_chunk, total_rows)
-  # Slice each dataframe in the list to get the subset of rows for the current chunk
-  chunk <- fut_coords[start_index:end_index,]
-  # Append the chunk to the list of chunks
-  coord_chunks[[i]] <- chunk
-}
-
-saveRDS(coord_chunks, '../data/coord_chunks.Rds')
-rm(ann_fut, fut_coords)
+rm(ann_fut, chunk)
 gc()
 # ------------------------------------------------------------------------------
 
 
 # Parallelize and run!
-options(future.globals.maxSize= 5000000000)
+options(future.globals.maxSize = 5000000000)
 plan(multicore, workers = 46)
 
-chunks %>% 
+chunks |>
   iwalk(\(chunk, chunk_idx){
      
-    # Calculate sigmas to analogs for each focal point 
-    sigma_dt <- seq(nrow(chunk[[1]])) %>% 
-      future_map_dfr(md_fun, .id = 'focal_id', .progress = TRUE)
+    # Calculate sigmas to analogs for each focal point
+    sigma_dt <- seq_len(nrow(chunk[[1]])) |>
+      future_map_dfr(md_fun, .id = "focal_id", .progress = TRUE)
     
     # Save as RDS
-    saveRDS(sigma_dt, paste0('../data/sigma_output/sigma_dt_',chunk_idx,'.rds'))  
+    saveRDS(sigma_dt,
+            paste0("../data/sigma_output/sigma_dt_", chunk_idx, ".Rds"))
     
     # Explicitly remove and free memory each iteration
     rm(sigma_dt)
