1405d8d7e7ab12dc5559910e06e874b89264b19a
tylerhoecker <hoecker.tyler@gmail.com>
2023-12-18 16:37:52 -0700

    output dir

--- code/01_calc_sigma.R
+++ code/01_calc_sigma.R
@@ -15,7 +15,7 @@ library(data.table)
 # https://gist.github.com/etiennebr/9515738#file-as-data-table-r
 source("code/as.data.table.r")
 
-# OR-WA boundary for crop ------------------------------------------------------
+# Boundary for crop ------------------------------------------------------
 climate_dir <- file.path("data", "climate")
 
 new_crs <- crs(rast(paste0(climate_dir, "/topoterra_2C_2000.tif")))
@@ -36,7 +36,6 @@ ann_fut <- grep("topoterra_2C_[0-9]{4}.tif",
 
 ann_fut <- lapply(ann_fut, FUN = as.data.table, xy = TRUE)
 
-
 # Historical normals (downscaled from TerraClimate normals)
 norm_hist <- grep("topoterra_hist_[0-9]{4}-[0-9]{4}.tif",
                   list.files(climate_dir),
@@ -50,15 +49,41 @@ rm(new_crs, w_states, r6)
 gc()
 #-------------------------------------------------------------------------------
 
-# Calculate sigma dissimilarity between all focal locations and n best analogs
+# Break the focal dataset into chunks
+# ------------------------------------------------------------------------------
+# Set the number of chunks
+num_chunks <- 300
+# Get the total number of rows in each dataframe
+total_rows <- nrow(ann_fut[[1]])
+# Calculate the number of rows in each chunk
+rows_per_chunk <- total_rows %/% num_chunks
+# Initialize an empty list to store chunks
+chunks <- list()
+
+# Iterate over the range of chunks
+for (i in 1:num_chunks) {
+  # Calculate the start and end indices for each chunk
+  start_index <- (i - 1) * rows_per_chunk + 1
+  end_index <- min(i * rows_per_chunk, total_rows)
+  # Get the subset of rows for the current chunk
+  chunk <- lapply(ann_fut,
+                  function(df) df[start_index:end_index, , drop = FALSE])
+  # Append the chunk to the list of chunks
+  chunks[[i]] <- chunk
+}
+rm(ann_fut, chunk)
+gc()
+# ------------------------------------------------------------------------------
+
+# Calculate sigma dissimilarity between all focal locations and analogs
 #-------------------------------------------------------------------------------
 # How many analogs randomly sampled from global pool?
 n_analogs <- 10000000
-#pt_i  <- 1000
-#chunk  <- chunks[[100]]
 
 # The MD/sigma dissimilarity function
-md_fun <- function(pt_i){
+# Testing: pt_i  <- 1000; chunk  <- chunks[[100]] # nolint
+
+md_fun <- function(pt_i) {
   
   # Build cov matrix for pt_i from 30 years of annual projected future data
   cov_i <- map(chunk, ~ .x[pt_i]) |>
@@ -129,33 +154,6 @@ md_fun <- function(pt_i){
   gc()
   return(out_dt)
 }
-
-# Break the focal dataset into chunks
-# ------------------------------------------------------------------------------
-# Set the number of chunks
-num_chunks <- 300
-# Get the total number of rows in each dataframe
-total_rows <- nrow(ann_fut[[1]])
-# Calculate the number of rows in each chunk
-rows_per_chunk <- total_rows %/% num_chunks
-# Initialize an empty list to store chunks
-chunks <- list()
-
-# Iterate over the range of chunks
-for (i in 1:num_chunks) {
-  # Calculate the start and end indices for each chunk
-  start_index <- (i - 1) * rows_per_chunk + 1
-  end_index <- min(i * rows_per_chunk, total_rows)
-  # Get the subset of rows for the current chunk
-  chunk <- lapply(ann_fut,
-                  function(df) df[start_index:end_index, , drop = FALSE])
-  # Append the chunk to the list of chunks
-  chunks[[i]] <- chunk
-}
-rm(ann_fut, chunk)
-gc()
-# ------------------------------------------------------------------------------
-
 
 # Parallelize and run!
 options(future.globals.maxSize = 5000000000)
