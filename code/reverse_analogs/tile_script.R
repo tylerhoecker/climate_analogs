19bcee234c12ca215df6cf80162529a636f72be3
souma4 <jeffrey.chandler435@gmail.com>
2026-02-23 09:09:13 -0700

    adding R example script and the script used to create input data from rasters

---
+++ code/reverse_analogs/tile_script.R
@@ -0,0 +1,80 @@
+library(tidyverse)
+library(data.table)
+library(doParallel)
+library(future)
+
+cpus_per_task <- as.integer(Sys.getenv("SLURM_CPUS_PER_TASK"))
+registerDoParallel(cpus_per_task)
+
+paste0("Number of processes: ", as.character(cpus_per_task))
+
+project_dir <- "INSERT/PATH"
+# project_dir = "/home/jeff/Github/ClimateAnalogues/climate_analogs"
+source(file.path(project_dir, "code", "src", "climate_analogs_R", "climate_analogs.R"))
+
+
+
+
+max_dist <- 500
+if (length(commandArgs(trailingOnly = TRUE)) == 0) {
+    stop("Stopping: Args must be supplied")
+} else if (length(commandArgs(trailingOnly = TRUE)) > 1) {
+    stop("Stopping: Only 1 arg can be supplied")
+} else {
+    tile_id <- commandArgs(trailingOnly = TRUE)[1]
+}
+
+
+annuals_future_path <- file.path(project_dir, "data", "reverse_analogs", "inputs", paste0("annuals_future_", tile_id, ".Rds"))
+normal_future_path <- file.path(project_dir, "data", "reverse_analogs", "inputs", paste0("normal_future_", tile_id, ".Rds"))
+analog_pool_path <- file.path(project_dir, "data", "reverse_analogs", "inputs", paste0("analog_pool_", tile_id, ".Rds"))
+annuals_future <- readRDS(annuals_future_path)
+normal_future <- readRDS(normal_future_path)
+analog_pool <- readRDS(analog_pool_path)
+
+
+try(
+    {
+        message("datasets for tile ", tile_id, " loaded")
+    },
+    silent = TRUE
+)
+
+message("loaded data")
+
+
+
+# This takes 4.5 hours to ~1.4m analogs across reverse_analogs points on 25 cores
+# calculate n_analog_use
+proportion_landscape <- 0.05
+n_analog_pool <- round(((2 * max_dist) / 0.270)^2 * proportion_landscape, digits = 0) |> as.numeric()
+
+tryCatch(
+    {
+        message("starting analogs tile ", tile_id)
+    },
+    error = function(e) {
+        message("starting analog calculation")
+    }
+)
+
+# library(profvis)
+# start = time()
+# profvis({
+# test first 12
+
+#  for i in 1:size(annuals_future,1)
+#    annuals_future[i] = annuals_future[i][1:12, :]
+#  end
+
+analog_results <- find_analogs(
+    annuals_future,
+    normal_future,
+    analog_pool,
+    c("aet", "def", "tmax", "tmin"),
+    n_analog_pool, # number of analog pixels to sample
+    100, # number of analogs to keep
+    0, # In KM!
+    max_dist, # In KM!
+    paste0(project_dir, "/data/reverse_analogs/outputs/reverse_analogs_", tile_id)
+)
